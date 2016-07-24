fb5b3f916d88e6e2441870efd41b224b06379017
Shashank Gupta <27392shashankgupta@gmail.com>
2016-07-25 02:32:06 +0530

    initializers and utils functions added

---
+++ initialization.py
@@ -0,0 +1,47 @@
+#!/usr/bin python
+
+# Utility function for parameter initialization in theano
+
+from theano import tensor as T
+import numpy as np
+
+def relu(n_inp, n_out):
+    raise NotImplmentedError
+
+
+def sigmoid(n_inp, n_out):
+    W = np.random.uniform(low = - 4 * np.sqrt(6.0/float(n_inp, n_out)), \
+                          high = 4 * np.sqrt(6.0/float(n_inp, n_out)), \
+                          size=(n_inp, n_out)).astpye(np.float32)
+    return theano.shared(W, borrow=True)
+
+
+def tanh(n_inp, n_out):
+    W = np.random.uniform(low = - np.sqrt(6.0/float(n_inp, n_out)), \
+                          high = np.sqrt(6.0/float(n_inp, n_out)), \
+                          size=(n_inp, n_out)).astpye(np.float32)
+    return theano.shared(W, borrow=True)
+
+
+def zeros(n_inp, n_out):
+    W = np.zeros((n_inp, n_out), dtype=theano.config.floatX)
+    return theano.shared(W, borrow=True)
+
+
+def ones(n_inp, n_out):
+    W = np.ones((n_inp, n_out), dtype=theano.config.floatX)
+    return theano.config(W, borrow=True)
+
+
+def get_value(x):
+    # get value from tensor x
+    return x.get_value()
+
+def set_value(x, val):
+    # set value of tensor x to val
+    x.set_value(val)
+
+
+
+
+
